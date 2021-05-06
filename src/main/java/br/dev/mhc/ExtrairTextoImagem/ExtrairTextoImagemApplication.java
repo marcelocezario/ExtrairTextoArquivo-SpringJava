588e88096cd31c08fd95b9b92ef4f5ca5be93cf7
Marcelo Cezário <marcelocezario@gmail.com>
2021-05-06 12:38:08 -0300

    Initial commit

---
+++ src/main/java/br/dev/mhc/ExtrairTextoImagem/ExtrairTextoImagemApplication.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.ExtrairTextoImagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtrairTextoImagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtrairTextoImagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/mhc/ExtrairTextoImagem/ExtrairTextoImagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.ExtrairTextoImagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtrairTextoImagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
